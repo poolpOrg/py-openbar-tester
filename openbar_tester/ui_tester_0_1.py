e507a2dc01a280d8becabb486f2759f804c2b136
Gilles Chehade <gilles@poolp.org>
2020-07-24 17:18:25 +0200

    add sessions

--- openbar_tester/ui_tester_0_1.py
+++ openbar_tester/ui_tester_0_1.py
@@ -1,12 +1,24 @@
 import openbar.routes
+import openbar.sessions
 import openbar.templates
 
 
 @openbar.routes.register("0.1", "frontend")
 def setup(app):
     app.get("/")(get_root)
+    app.get("/session-reset")(get_session_reset)
 
 
 @openbar.templates.render("test.html")
 def get_root():
-    return {'test' : 42}
+    session = openbar.sessions.current()
+    session['counter'] = session.get('counter', 0) + 1
+    session.save()
+
+    return {'counter' : session['counter']}
+
+def get_session_reset():
+    session = openbar.sessions.current()
+    session.delete()
+
+    return {}
