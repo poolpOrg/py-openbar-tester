e21316a3fbb1f8ac59b4f4e11002794632158f29
Gilles Chehade <gilles@poolp.org>
2020-07-24 16:08:05 +0200

    database example

--- openbar-tester/backend.py
+++ openbar-tester/backend.py
@@ -1,13 +1,19 @@
 import openbar.routes
 import openbar.params
 
+from . import db_tester
+
 
 @openbar.routes.register("0.1", "backend")
 def setup(app):
-    app.post("/")(post_root)
+    app.get("/")(post_root)
 
 
 def post_root():
     with openbar.params.json() as j:
         test = j.string('test', default=None)
+
+    with db_tester.connector("tester-database") as conn:
+        conn.test()
+
     return {'test': test}
